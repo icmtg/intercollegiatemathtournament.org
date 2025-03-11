de1317d4bd41361230e2fc53f1fdf2c131c4024e
Oliver Ni <oliverni@berkeley.edu>
2025-03-10 20:45:10 -0700

    feat: ci

--- default.nix
+++ default.nix
@@ -1,12 +1,11 @@
-{ lib, stdenvNoCC, tailwindcss, zola, baseUrl ? null }:
+{ lib, stdenvNoCC, just, tailwindcss_4, zola, baseUrl ? null }:
 
 stdenvNoCC.mkDerivation {
   pname = "berkeley.mt";
   version = "1.0.0";
   src = ./.;
-  nativeBuildInputs = [ tailwindcss zola ];
+  nativeBuildInputs = [ just tailwindcss_4 zola ];
   buildPhase = ''
-    tailwindcss -i ./static/input.css -o ./static/style.css
-    zola build -o "$out" ${lib.optionalString (baseUrl != null) "--base-url ${baseUrl}"}
+    just build -o "$out" ${lib.optionalString (baseUrl != null) "--base-url ${baseUrl}"}
   '';
 }
