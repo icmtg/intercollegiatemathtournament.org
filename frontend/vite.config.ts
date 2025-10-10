f46dacb5077347a688c64c7eddd3a7829d39de4b
Oliver Ni <oliverni@berkeley.edu>
2025-10-10 02:56:04 -0700

    fix: deprecation warning with tanstack router vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tailwindcss()],
+  plugins: [tanstackRouter(), react(), tailwindcss()],
   server: {
     port: 5173,
     proxy: {
